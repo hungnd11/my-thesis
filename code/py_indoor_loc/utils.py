f1bd84d70dc9082b00ee6b2ff81ab80ffcdaa614
hungnd11 <hungnd1235@gmail.com>
2023-09-04 16:35:14 +0700

    LiFS: add step counting

--- code/py_indoor_loc/utils.py
+++ code/py_indoor_loc/utils.py
@@ -39,3 +39,68 @@ def inverse_rotate(xs: np.ndarray, ys: np.ndarray,
   ir_xs = xs * np.cos(a) - ys * np.sin(a)
   ir_ys = xs * np.sin(a) + ys * np.cos(a)
   return ir_xs, ir_ys
+
+
+class DataListingUtil(object):
+
+  def __init__(self, train_test_assignment: pd.DataFrame,
+               experiment_setup: pd.DataFrame, data_dir: str | Path):
+    self.train_test_assignment = train_test_assignment
+    self.experiment_setup = experiment_setup
+
+    if isinstance(data_dir, str):
+      self.data_dir = Path(data_dir)
+    else:
+      self.data_dir = data_dir
+
+  def list_unlabeled_tracks(self, site_id: str, floor_id: str,
+                            p: float) -> list[str]:
+    assert p >= 0 and p <= 1
+
+    experiment_setup = self.experiment_setup
+
+    return experiment_setup.loc[(experiment_setup["site_id"] == site_id) &
+                                (experiment_setup["floor_id"] == floor_id) &
+                                (experiment_setup["dataset"] == "unlabeled") &
+                                (experiment_setup["supervision_pct"] == p),
+                                "track_id"].values.tolist()
+
+  def list_labeled_tracks(self, site_id: str, floor_id: str,
+                          p: float) -> list[str]:
+    assert p >= 0 and p <= 1
+
+    experiment_setup = self.experiment_setup
+
+    return experiment_setup.loc[(experiment_setup["site_id"] == site_id) &
+                                (experiment_setup["floor_id"] == floor_id) &
+                                (experiment_setup["dataset"] == "labeled") &
+                                (experiment_setup["floor_id"] == p),
+                                "track_id"].values.tolist()
+
+  def list_train_tracks(self, site_id: str, floor_id: str):
+    return self._list_track_by_dataset(site_id, floor_id, dataset="train")
+
+  def list_test_track(self, site_id: str, floor_id: str):
+    return self._list_track_by_dataset(site_id, floor_id, dataset="test")
+
+  def _list_track_by_dataset(self, site_id: str, floor_id: str, dataset: str):
+    train_test_assignment = self.train_test_assignment
+    return train_test_assignment.loc[
+        (train_test_assignment["site_id"] == site_id) &
+        (train_test_assignment["floor_id"] == floor_id) &
+        (train_test_assignment["dataset"] == dataset),
+        "track_id"].values.tolist()
+
+  def list_train_files(self, site_id: str, floor_id: str):
+    return self.list_files(site_id, floor_id,
+                           self.list_train_tracks(site_id, floor_id))
+
+  def list_test_files(self, site_id: str, floor_id: str):
+    return self.list_files(site_id, floor_id,
+                           self.list_train_tracks(site_id, floor_id))
+
+  def list_files(self, site_id: str, floor_id: str, track_list: list[str]):
+    return [
+        self.data_dir / site_id / floor_id / (track_id + ".txt")
+        for track_id in track_list
+    ]
